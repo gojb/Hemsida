b07a75ccb1a4f00c1a2f987d473f31e242fabb79
Jakob <gojbmail@gmail.com>
2015-07-27 16:50:50 +0200

    Signed-off-by: Jakob <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
 		console.log($width + "  " + ($width+($("#header").css("padding-left").replace("px","")*2)));
 		console.log($("#header").css("padding-left").replace("px",""));
 		console.log(Math.ceil($("#header").css("padding-left").replace("px",""))*2);
-		bredd=$width+($("#header").css("padding-left").replace("px","")*2);
+		bredd=$width+(Math.ceil($("#header").css("padding-left").replace("px",""))*2);
 		resize();
 	});
 	$('.prg').mouseenter(function(){
