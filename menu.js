1d99fcf3e5914433d19c186918b2aa741ef057ad
Jakob <gojbmail@gmail.com>
2015-07-27 16:32:33 +0200

    Signed-off-by: Jakob <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
 //		});
 		$width += $("#logo").outerWidth();
 		console.log($width + "  " + ($width+110));
-		console.log($("#logo").css("padding-left"));
+		console.log($("#header").css("padding-left"));
 		bredd=$width+110;
 		resize();
 	});
