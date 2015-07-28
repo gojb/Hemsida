9d261732971bdcbb7d218e29144442ca7faf488b
Glenn <gojbmail@gmail.com>
2015-07-28 23:20:39 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -14,38 +14,36 @@ $(document).ready(function()
 			}
 		});
 	});
-	function Cookie(){
-		function setCookie(name, value, exdays) {
-			var d = new Date();
-			d.setTime(d.getTime() + (exdays*24*60*60*1000));
-			var expires = "expires="+d.toUTCString();
-			document.cookie = name + "=" + value + "; " + expires + "; path=/";
-		}
-
-		function getCookie(cookiename) {
-			var name = cookiename + "=";
-			var ca = document.cookie.split(';');
-			for(var i=0; i<ca.length; i++) {
-				var c = ca[i];
-				while (c.charAt(0)==' ') c = c.substring(1);
-				if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-			}
-			return "";
-		}
-
-		function checkCookie() {
-			var user = getCookie("lang");
-			if (user != "") {
-				alert("Welcome again " + user);
-			} else {
-				if (user != "" && user != null) {
-					setCookie("username", prompt("Set Username"), 2);
-				}
-			}
-		}
-		checkCookie();
+		});
+function Cookie(){
+	function setCookie(name, value, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = name + "=" + value + "; " + expires + "; path=/";
 	}
 
+	function getCookie(cookiename) {
+		var name = cookiename + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+		}
+		return "";
+	}
 
-		});
+	function checkCookie() {
+		var user = getCookie("lang");
+		if (user != "") {
+			alert("Welcome again " + user);
+		} else {
+			if (user != "" && user != null) {
+				setCookie("username", prompt("Set Username"), 2);
+			}
+		}
+	}
+	checkCookie();
+}
 setTimeout(Cookie(),10000);
