50a95b40d754573e3d59db1388c399280fefa57b
Jakob <gojbmail@gmail.com>
2015-07-28 22:51:36 +0200

    Signed-off-by: Jakob <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -3,5 +3,12 @@ $(document).ready(function()
     $(".bild").error(function(){
         $(this).attr('src', '/images/rect8147.png');
     });
-    
+    $(function linkActive() {
+		$("a").each(function() {
+			if (window.location.href == (this.href)) {
+				$(this).addClass("active");
+				$(this).parents("ul").siblings("a").addClass("active");
+			}
+		});
+	});
 });
