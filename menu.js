63561dbebcd98e791f156d5f2ae55b3174307404
Jakob <gojbmail@gmail.com>
2015-07-28 22:54:01 +0200

    Signed-off-by: Jakob <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -3,6 +3,9 @@ $(document).ready(function()
     $(".bild").error(function(){
         $(this).attr('src', '/images/rect8147.png');
     });
+    $(".menucss").error(function(){
+        $(this).attr('href', '/menu.css');
+    });
     $(function linkActive() {
 		$("a").each(function() {
 			if (window.location.href == (this.href)) {
