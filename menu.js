4904f75c930e3daf4699c4f2f5fd87ad907d8f9b
Glenn <gojbmail@gmail.com>
2015-07-28 22:56:03 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -1,7 +1,11 @@
-$(document).ready(function()
-{
-    $(".bild").error(function(){
-        $(this).attr('src', '/images/rect8147.png');
-    });
-    
-});
+$(document).ready(function(){
+	$(".bild").error(function(){
+		$(this).attr('src', '/images/rect8147.png');
+	});
+
+	function Cookie(){
+		console.log("Cookie!");
+	}
+	Cookie();
+	
+		});
