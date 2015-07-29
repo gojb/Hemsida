76b6f02e7b7616e079391e36644c77e20fd73a6b
Jakob <gojbmail@gmail.com>
2015-07-29 09:40:12 +0200

    Signed-off-by: Jakob <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -1,13 +1,6 @@
-$(document).ready(function()
-		{
-	$(".bild").error(function(){
-		$(this).attr('src', '/images/rect8147.png');
-	});
-	$(".menucss").error(function(){
-		$(this).attr('href', '/menu.css');
-	});
+$(document).ready(function(){
 	$(function linkActive() {
-		$("a").each(function() {
+		$("nav a").each(function() {
 			if (window.location.href == (this.href)) {
 				$(this).addClass("active");
 				$(this).parents("ul").siblings("a").addClass("active");
