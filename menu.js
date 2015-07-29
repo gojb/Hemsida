ed7d9a60bc2e2e15da7879ebadb01634c91d296f
Glenn <gojbmail@gmail.com>
2015-07-29 19:52:58 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -8,15 +8,16 @@ $(document).ready(function(){
 		});
 	});
 		});
-function Cookie(){
+function Cookie(CookieVar){
 	console.log("Cookies!!")
-	
+	var namn = CookieVar+"=";
 	cookieArray = document.cookie.split(";");
 	for(var i = 0; i < cookieArray.length; i++){
-		if(cookieArray[i].indexOf("lang=")!==-1){
-			var cookie = cookieArray[i].substring("lang=".length, cookieArray[i].length+1);
+		if(cookieArray[i].indexOf(namn)!==-1){
+			var cookie = cookieArray[i].substring(namn.length, cookieArray[i].length+1);
+			console.log(cookie);
 		}else {
-			console.log("Error!!", "font-weight: 70px; color: red;")
+			console.log("%c Error!!", "font-weight: 70px; color: red;")
 		}
 	}
 	
@@ -54,4 +55,4 @@ function Cookie(){
 //		}
 //	}
 }
-setTimeout(Cookie(),1000);
+setTimeout(Cookie("lang"),1000);
