c1ec4b223fad5aca508da525d1a7bb24c1bc08cb
Glenn <gojbmail@gmail.com>
2015-07-30 13:57:51 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

---
+++ chat/chat.js
@@ -0,0 +1,84 @@
+
+var socket;
+
+function connect() {
+	if ('WebSocket' in window) {
+		socket = new WebSocket("ws://wildfly-gojb.rhcloud.com:8000/");
+	}  else {
+		Console.log('Error: WebSocket stöds inte.');
+	}
+};
+
+socket.onopen = function () {
+	var person = prompt("Vad heter du?", "");
+	Chat.socket.send(person);
+	Console.log('Info: Anslutning Öppnad');
+	document.getElementById('knapp').setAttribute('disabled','disabled');
+	document.getElementById('chat').onkeydown = function(event) {
+		if (event.keyCode == 13) {
+			sendMessage();
+		}
+	};
+};
+
+socket.onclose = function () {
+	document.getElementById('chat').onkeydown = null;
+	Console.log('Info: WebSocket stängd.');
+	document.getElementById('knapp').removeAttribute('disabled');
+};
+
+socket.onmessage = function (message) {
+	Console.log(message.data);
+
+};
+
+
+function sendMessage() {
+	var message = document.getElementById('chat').value;
+	if (message != '') {
+		Chat.socket.send(message);
+		document.getElementById('chat').value = '';
+	}
+};
+
+var Console = {};
+
+Console.log = (function(message) {	
+	var console = document.getElementById('console');
+	var p = document.createElement('p');
+	p.style.wordWrap = 'break-word';
+	p.innerHTML = message;
+	console.appendChild(p);
+	while (console.childNodes.length > 25) {
+		console.removeChild(console.firstChild);
+	}
+	console.scrollTop = console.scrollHeight;
+	var title = document.title;
+	document.title = "Nytt meddelande";
+	playSound();
+	setTimeout(function(){document.title = title},3000);
+
+});
+
+function playSound(){   
+
+	document.getElementById('ljud').play();
+}
+function sound(){
+	if(document.getElementById('ljud').muted == false){
+		document.getElementById('knapp2').innerText = "Ljudet är av";
+		document.getElementById('ljud').muted = true;
+	}
+	else{
+		document.getElementById('knapp2').innerText = "Ljudet är på";
+		document.getElementById('ljud').muted = false;
+	}
+}
+function start(){
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		document.getElementById('ljud').controls = true;
+	}
+}
+start();
+
+
