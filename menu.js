8f9a7f56cf04507df4fd496d4372e762e6b1a4ed
Glenn <gojbmail@gmail.com>
2015-07-31 12:43:55 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -76,7 +76,7 @@ $(document).ready(function(){
 		              /*fortfarande 7*/" so you can chat with everyone that are online at the moment.",
 		              /*8*/"Programs",
 		              /*9*/"<input type='text' placeholder='Blchh'"+
-				"id='chat' /><button type='button' onclick='connect('svenska')' id='knapp'>Anslut</button>"+
+				"id='chat' />"/*<button type='button' onclick='connect('svenska')' id='knapp'>Anslut</button>*/+
 			"<button type='button' onclick='sound('svenska')' id='knapp2'>Ljudet är av</button>"
 		              ]/*I htmlId är 0-3 i menu.html, 4-6 på startsidan, 7 är chat, 8 är "program" i menyn*/
 		for(var i = 0; i < htmlId.length; i++){
