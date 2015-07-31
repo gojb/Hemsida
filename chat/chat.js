6e4ae4ef76dcbfccdd62b857a1ed3066467b59b4
Jakob <jakob_b99@live.se>
2015-07-31 18:41:28 +0200

    Signed-off-by: Jakob <jakob_b99@live.se>

--- chat/chat.js
+++ chat/chat.js
@@ -58,7 +58,7 @@ $(document).ready(function(){
 
 		if(lang=='uk'){
 			if ('WebSocket' in window) {
-				socket = new WebSocket("ws://wildfly-gojb.rhcloud.com:8000/chat/");
+				socket = new WebSocket("ws://wildfly-gojb.rhcloud.com:8000/chat");
 			}  else {
 				Console.log('Error: WebSocket is not supported.');
 			}
