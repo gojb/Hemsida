71f2b2a7733c0b62b546e6d58bdb9113b3cf3baa
Glenn <gojbmail@gmail.com>
2015-07-31 11:44:40 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -75,7 +75,7 @@ $(document).ready(function(){
 		              /*7*/"You can here connect to GoJb's chat. The program uses websocket to comunicate with  our servers" +
 		              /*fortfarande 7*/" so you can chat with everyone that are online at the moment.",
 		              /*8*/"Programs",
-		              /*9*/"<input type='text'placeholder='Enter your message here and press ENTER to send'"
+		              /*9*/"<input type='text'placeholder='Enter your message here and press ENTER to send'"+
 	              /*fortfarande 9*/"id='chat' />"/*<button type='button' onclick='connect()' id='knapp'>Connect</button>"+*/
 //		              /*fortfarande 9*/"<button type='button' onclick='sound()' id='knapp2'>The sound is off</button>"
 		              ]/*I htmlId är 0-3 i menu.html, 4-6 på startsidan, 7 är chat, 8 är "program" i menyn*/
