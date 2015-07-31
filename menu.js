583efb6f46dbe87e4a5c70c4a6c3630c1572e16e
Glenn <gojbmail@gmail.com>
2015-07-31 17:26:48 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -77,7 +77,7 @@ $(document).ready(function(){
 		              /*8*/"Programs",
 		              /*9*/"<input type='text' placeholder='Enter a message and press ENTER to send' id='chat' /> <button type='button' " +
 		              /*fortfarande 9*/" id='knapp'>Connect</button><button type='button' id='knapp2'> The sound is disabled</button>",
-		              /*10*/"<h1>Error 404 - Page not found</h1><p>The page you are trying to reach can not be found</p>"
+		              /*10*/"<h1>Error 404 - Page not found</h1><p>The page you are trying to reach was not found</p>"
 		              ]/*I htmlId är 0-3 i menu.html, 4-6 på startsidan, 7 är chat, 8 är "program" i menyn*/
 		for(var i = 0; i < htmlId.length; i++){
 			$("#"+i).html(htmlId[i]);
