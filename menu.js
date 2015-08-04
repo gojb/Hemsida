cdf0d40e58f48c3648fd1665fe4684a2c5cda660
Jakob <jakob_b99@live.se>
2015-08-04 21:14:09 +0200

    Signed-off-by: Jakob <jakob_b99@live.se>

--- menu.js
+++ menu.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('body').addClass("bred");
 	$('.header').children().each(function(){
 		console.log($(this).outerWidth(true));
+		bredd+=$(this).outerWidth(true);
 	});
 	res();
 	$('.smal .meny').css("left", "-250px");
