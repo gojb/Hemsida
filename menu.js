7ced06224359015057542244b4a1ef1a64acaa78
Glenn <gojbmail@gmail.com>
2015-08-04 10:33:11 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- menu.js
+++ menu.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 			if(cookieArray[i].indexOf(namn)!==-1){
 
 				var cookie = cookieArray[i].substring(namn.length, cookieArray[i].length+1);
-
+				console.log(cookieArray[i]);
 				if(cookie.indexOf("=")!==-1){
 					console.log(cookieArray[i].substring(namn.length+1, cookieArray[i].length+1));
 					cookie=cookieArray[i].substring(namn.length+1, cookieArray[i].length+1 + "   = finns");
