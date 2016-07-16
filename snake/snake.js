93c224eaafdd6a15fc98c2b54a47f69836bf868a
Jakob <jakob_b99@live.se>
2016-07-16 18:09:55 +0200

    Signed-off-by: Jakob <jakob_b99@live.se>

--- snake/snake.js
+++ snake/snake.js
@@ -1,4 +1,4 @@
-var ctx = $('.snakeruta').getContext("2d");
+var ctx = $('.snakeruta')[0].getContext("2d");
 var pixelstorlek = 10;
 $('.snakeruta').width = pixelstorlek*50+1;
 $('.snakeruta').height =  pixelstorlek*50+1;
